3f15c136880078b519a122d6d008766ce5841260
Laia Capellà Solé <laia.capella01@estudiant.upf.edu>
2026-05-11 15:34:39 +0200

    divisio

--- calculator.py
+++ calculator.py
@@ -3,3 +3,9 @@ def add(a, b):
 
 def sub(a, b):
     return a - b
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "No es pot dividir per 0"
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,11 @@
-from calculator import add, sub
+from calculator import add, sub, divide
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_sub():
     assert sub(10, 4) == 6
+
+def test_divide():
+    assert divide(12, 4) == 3
+    assert divide(20, 2) == 10
