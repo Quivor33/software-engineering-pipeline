f6b888f28ccb39884c15a8e90597fc8f778e8f81
MarcC845 <167287957+MarcC845@users.noreply.github.com>
2026-05-11 15:23:26 +0200

    test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,8 +6,3 @@ def test_add():
 def test_sub():
     assert sub(10, 4) == 6
     
-from calculator import add, sub, multiply
-
-def test_multiply():
-    assert multiply(3, 4) == 12
-    assert multiply(0, 5) == 0
