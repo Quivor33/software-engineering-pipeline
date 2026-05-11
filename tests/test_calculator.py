fb590536d50342a1fff838896d1e35f60fceece7
Alex-gar-her <alex.garcia11@estudiant.upf.edu>
2026-05-11 15:26:25 +0200

    Test sqrt fet

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,11 @@
-from calculator import add, sub
+from calculator import add, sub, sqrt
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_sub():
     assert sub(10, 4) == 6
+
+
+def test_sqrt():
+    assert sqrt(4) == 2
